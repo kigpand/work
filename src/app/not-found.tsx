749300716ab3e434728e90d628b8a6ed7e328abb
김지훈 <kigpand@gmail.com>
2023-04-14 18:16:06 +0900

    404 page 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import styles from "./notFound.module.scss";
+
 export default function NotFountPage() {
-  return <div>페이지를 찾을수 없습니당</div>;
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "rgb(88, 88, 88)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        flexDirection: "column",
+        fontSize: "25px",
+      }}
+    >
+      Page Not Found.
+    </div>
+  );
 }
